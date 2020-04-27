84422433affafb902c128303b980f41f61d4cd66
Edgar-Zamora <edgarzamora2012@hotmail.com>
2020-04-27 16:12:29 -0700

    added parameter to csr function.

--- R/course_success_rate.R
+++ R/course_success_rate.R
@@ -1,15 +1,26 @@
 #' Calculating course success rates
 #'
 #' @param data
+#' @param rate Determine the output type. Default is the raw output but can be changed to 'percent' to get
+#'             pass rate as a percentage
 #'
-#' @return
+#' @return A dataframe of all the key metrics for course success rates
 #' @export
 #'
 #' @examples
+#' \donotrun{
+#' transcript_tbl %>%
+#'    course_success_rates()
+#'
+#' transcript_tbl %>%
+#'    course_success_rates(rate = 'percent')
+#'
+#' }
 #'
 #'
-course_success_rate <- function(data) {
-  {{data}} %>%
+course_success_rate <- function(data, rate = 'raw') {
+
+  core_data <- {{data}} %>%
     clean_dw_transcript() %>%
     mutate(outcome = case_when(
       gr == "W" ~ "Withdraw",
@@ -27,7 +38,16 @@ course_success_rate <- function(data) {
     mutate(withdraw = if_else(is.na(Withdraw), 0, as.numeric(Withdraw)),
            passed = if_else(is.na(Passed), 0, as.numeric(Passed)),
            failed = if_else(is.na(Failed), 0, as.numeric(Failed)),
-           total_stu = withdraw + passed + failed,
-           pass_rate = round(passed/total_stu * 100, 1)) %>%
-    select(year, dept_div, course_num, course_title, withdraw, passed, failed, total_stu, pass_rate, cr, dist_ed)
+           total_stu = withdraw + passed + failed) %>%
+    select(year, dept_div, course_num, course_title, withdraw, passed, failed, total_stu, cr, dist_ed)
+
+
+  if(rate == 'percent'){
+    core_data <- core_data %>%
+      mutate(pass_rate = round(passed/total_stu * 100, 1)) %>%
+      select(year, dept_div, course_num, course_title, withdraw, passed, failed, pass_rate, total_stu, cr, dist_ed)
+  }
+
+  return(core_data)
+
 }
