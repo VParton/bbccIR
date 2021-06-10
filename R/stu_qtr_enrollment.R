1802dbf2d36b4f293d8e8fe5e463d75b178905e9
Edgar Zamora <edgarzamora2012@hotmail.com>
2021-06-10 13:33:25 -0700

    Solution to multiple quarter issue
    A temporary fix for #1. We will need to consider whether to limit the number of quarters that can be provided as it currently is.

--- R/stu_qtr_enrollment.R
+++ R/stu_qtr_enrollment.R
@@ -15,49 +15,43 @@
 #'
 #'
 
-stu_not_registred_nxt_qtr <- function(current_qtr, nxt_qtr) {
-
+stu_not_registred_nxt_qtr <- function(current_qtr, summer_qtr = NULL) {
+  
   # Making database connection
   con <- dbConnect(odbc::odbc(), "R Data")
-
-
-  # Loading tables
-  current_qtr <- tbl(con, "dbo_enrollment") %>%
-    filter(YearQuarterID == {{current_qtr}},
-           EnrolledCredits > 0) %>%
-    select(SID, YearQuarterID) %>%
-    collect() %>%
-    clean_names() %>%
-    distinct_all()
-
-
-  desired_qtr <- tbl(con, "dbo_enrollment") %>%
-    filter(YearQuarterID == {{nxt_qtr}},
-           EnrolledCredits > 0) %>%
-    select(SID) %>%
-    collect() %>%
-    clean_names() %>%
-    mutate(nxt_qtr = "yes") %>%
-    distinct_all()
-
-  stu_not_registred <- current_qtr %>%
-    anti_join(desired_qtr, by = "sid") %>%
-    rename(
-      current_qtr = year_quarter_id
-    )  %>%
-    left_join(tbl(con, "dbo_Student") %>%
-                select(SID, FirstName, LastName, DaytimePhone, EveningPhone, Email,
-                       NTUserName, EducationalProgramID, StudentIntentID) %>%
-                collect() %>%
-                clean_names(), by = "sid") %>%
-    left_join(tbl(con, "PROGRAM NAME LU") %>%
-                collect() %>%
-                clean_names(), by = c("educational_program_id" = "program_code")) %>%
-    filter(!str_detect(str_trim(nt_user_name), "\\.")) %>%
-    mutate(bbcc_email = paste0(str_trim(nt_user_name), "@bigbend.edu")) %>%
-    select(-nt_user_name) %>%
-    distinct_all()
-
-  return(stu_not_registred)
-
+  
+  output <- tbl(con, "dbo_Student") %>% 
+    filter(StudentIntentID %in% c("A", "B", "F", "G", "M")) %>% 
+    select(SID, FirstName, LastName, DaytimePhone, EveningPhone, Email, YRQLastAttended, StudentIntentID,
+           EducationalProgramID, YRQTargetGrad, CollLevCREarned) %>% 
+    collect() %>% 
+    clean_names() %>% 
+    filter(is.na(yrq_target_grad)) %>% 
+    inner_join(tbl(con, "dbo_Enrollment") %>% 
+                 filter(EnrolledCredits > 0) %>% 
+                 select(SID, YearQuarterID) %>% 
+                 collect() %>% 
+                 clean_names(), by = c('sid', 'yrq_last_attended' = 'year_quarter_id')) %>% 
+    distinct_all() %>% 
+    left_join(tbl(con, "PROG CIP and NAME") %>% 
+                collect() %>% 
+                clean_names() %>% 
+                select(program_code, program_title_general), by = c('educational_program_id' = 'program_code'))  %>% 
+    left_join(tbl(con, "YRQ LU") %>% 
+                collect() %>% 
+                clean_names() %>% 
+                select(yrq, year_qtr), by = c("yrq_last_attended" = "yrq")) %>% 
+    distinct_all() %>% 
+    arrange(sid)
+  
+  
+  # Inclusion of summer quarter
+  if(is.null(summer_qtr)){
+    output %>% 
+      filter(yrq_last_attended == {{current_qtr}})
+  } else {
+    output %>% 
+      filter(yrq_last_attended == {{current_qtr}} | yrq_last_attended == {{summer_qtr}})
+  }
 }
+
