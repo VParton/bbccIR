987d11635d579a1a0a57fc05b030461de96ed66a
Edgar Zamora <edgarzamora2012@hotmail.com>
2021-08-10 09:55:51 -0700

    Update to rs_demographics
    Adding functionality to allow selection of different demographics.

--- R/rs_demographics.R
+++ R/rs_demographics.R
@@ -2,8 +2,9 @@
 #'
 #' @param data The student table saved as an object
 #' @param high_school_name You can write out the entire name of the high school or match based on given name.
+#' @param demographic Choose which demographic (sex or race/ethnicity) to breakout by.
 #'
-#' @return Returns a tibble where ethnic groups are grouped according to typical categories (A/W, HUG) for each year and high school selected.
+#' @return Returns a tibble where each high school in a given year is broken out by the demographic choosen.
 #' @export
 #'
 #' @examples
@@ -12,38 +13,52 @@
 #'  rs_demographics(high_school_name = 'Eph')
 #'
 #'  student_tbl %>%
-#'     rs_demographic() %>%
 #'     filter(year == "B78")
+#'     rs_demographic(demographic = sex,
+#'                    high_school_name == "Othello")
 #'
 
-  rs_demographics <- function(data, high_school_name = '') {
+rs_demographics <- function(data, demographic = NULL, high_school_name = '') {
+  
+  con <- dbConnect(odbc::odbc(), "R Data")
+  
+  high_school_lu <- tbl(con, "dbo_HighSchool") %>%
+    select(HighSchoolID, HighSchoolName) %>%
+    collect() %>%
+    clean_names() %>%
+    mutate(high_school_id = str_trim(high_school_id),
+           high_school_name = str_to_title(high_school_name))
+  
+  rs_data <- {{data}} %>%
+    select(sid, year, dual_enroll, sex, race_ethnic_code, hi_schl) %>%
+    left_join(high_school_lu, by = c("hi_schl" = "high_school_id")) %>%
+    filter(dual_enroll %in% c("1")) %>%
+    mutate(race_ethnic_code = case_when(race_ethnic_code %in% c("1", "6") ~ "A/W",
+                                        is.na(race_ethnic_code) ~ "Unknown",
+                                        TRUE ~ "HUG")) %>% 
+    distinct_all() %>% 
+    count(year, {{demographic}}, high_school_name) %>% 
+    group_by(year, high_school_name) %>% 
+    mutate(percentage = round(n/sum(n), 2)) %>% 
+    rename(
+      count = n
+    ) %>% 
+    arrange(year, high_school_name, {{demographic}})
+  
+  
+  
+  # Accounting for if a user does not provide a high school
+  if(stri_isempty(high_school_name)) {
+    
+    rs_data <- rs_data
+    
+  } else {
+    
+    rs_data <- rs_data %>%
+      filter(str_detect(high_school_name, {{high_school_name}}))
+  }
+  
+  
+  return(rs_data)
+}
 
-    con <- dbConnect(odbc::odbc(), "R Data")
-
-    high_school_lu <- tbl(con, "dbo_HighSchool") %>%
-      select(HighSchoolID, HighSchoolName) %>%
-      collect() %>%
-      clean_names() %>%
-      mutate(high_school_id = str_trim(high_school_id),
-             high_school_name = str_to_title(high_school_name))
-
-    all_rs_data <- {{data}} %>%
-      select(sid, year, dual_enroll, race_ethnic_code, hi_schl) %>%
-      left_join(high_school_lu, by = c("hi_schl" = "high_school_id")) %>%
-      filter(dual_enroll %in% c("1")) %>%
-      distinct_all() %>%
-      mutate(race_ethnic_code = case_when(race_ethnic_code %in% c("1", "6") ~ "A/W",
-                                          is.na(race_ethnic_code) ~ "Unknown",
-                                          TRUE ~ "HUG")) %>%
-      count(year, race_ethnic_code, high_school_name)
-
-    if(stri_isempty(high_school_name)) {
-
-      all_rs_data
-
-      } else {
-
-        all_rs_data %>%
-          filter(str_detect(high_school_name, {{high_school_name}}))
-      }
-    }
